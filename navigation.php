df2db6cc5c05085c72736c579de9fb99df8b41bb
JoachimgGJKopp <JoachimGJ.Kopp@gmail.com>
2024-08-06 20:24:38 +0200

    first commit

---
+++ navigation.php
@@ -0,0 +1,101 @@
+
+    <div>
+
+            <li><a href="index.php">Home&nbsp;&nbsp;</a></li>
+
+            <li><span>Équipes</span>
+            <ul>
+                <li><a href="mannschaft.1.php">1. Mannschaft</a></li>
+                <li><a href="mannschaft.2.php">2. Mannschaft</a></li>
+                <li><a href="mannschaft.3.php">3. Mannschaft</a></li>
+                <li><a href="mannschaft.4.php">4. Mannschaft</a></li>
+                <li><a href="mannschaft.5.php">5. Mannschaft</a></li>
+            </ul>
+            </li>
+
+            <li><span>Verein</span>
+        	<ul>
+                <li><a href="vorstand.php">Vorstand</a></li>
+                <li><a href="adresse 2.php">Adresse / Anfahrt</a></li>
+                <li><a href="info.php">Informationen</a></li>
+                <li><a href="sponsoren.php">Sponsoren</a></li>
+                <li><a target="_blank"href="dokumente/Satzung2023.pdf">Satzung</a></li>
+                <li><a target="_blank"href="dokumente/MAntragEinwillBeitragso2022.pdf">Mitgliedsantrag</a></li>
+                <li><a target="_blank"href="dokumente/Datenschutzhinweis 2022.pdf">Datenschutzhinweis</a></li>
+                <li><a target="_blank"href="dokumente/Platzordnung.pdf">Platzordnung</a></li>
+                <li><a target="_blank"href="dokumente/Bewirtungsordnung.pdf">Bewirtungsordnung</a></li>
+		    </ul>
+            </li> 
+
+            <li><span>Turniere</span>
+                <ul>
+                    <li><a href="images/Turnierplan2024.pdf">Turnierplan 2024</a></li>
+                    <li><a href="turniere2.php">Sommercup</a></li>
+                </ul>
+            </li>
+
+            <li><span>Ergebnisse</span>
+            <ul>
+		        <li><a href="ergebnis50.php">Fr&uuml;hlingsturnier 2024</a></li>
+                <li><a href="ergebnis49.php">Jubil&auml;umsturnier 2023</a></li>
+                <li><a href="ergebnis48.php">Herbstturnier 2023</a></li>   
+	            <li><a href="ergebnis47.php">Badische Meisterschaften 2023</a></li>
+	            <li><a href="ergebnis46.php">Fr&uuml;hlingsturnier 2023</a></li> 
+                <li><a href="ergebnis45.php">Jubil&auml;umsturnier 2022</a></li>
+                <li><a href="ergebnis44.php">Herbstturnier 2022</a></li>
+                <li><a href="ergebnis43.php">Jubil&auml;umsturnier 2019</a></li>
+                <li><a href="ergebnis42.php">Herbstturnier 2019</a></li>
+	            <li><a href="ergebnis41.php">Badische Meisterschaften 2019</a></li>
+	            <li><a href="ergebnis40.php">Fr&uuml;hlingsturnier 2019</a></li>  
+            </ul>
+            </li>
+
+            <li><span>Galerien</span>
+            <ul>
+                <li><a href="galerie73.php">Fr&uuml;hlingsturnier 2024</a></li>
+	            <li><a href="galerie68.php">Jubil&auml;umsturnier 2023</a></li>
+                <li><a href="galerie65.php">Oktoberfest 2023</a></li>
+                <li><a href="galerie62.php">Herbstturnier 2023</a></li>
+                <li><a href="galerie60.php">Nocturne 2023</a></li>
+                <li><a href="galerie58.php">Badische Meisterschaften 2023</a></li>
+                <li><a href="galerie55.php">2. Großspieltag RL Süd 2023</a></li>
+                <li><a href="galerie52.php">Fr&uuml;hlingsturnier 2023</a></li>
+                <li><a href="galerie48.php">Jubil&auml;umsturnier 2022</a></li>
+                <li><a href="galerie46.php">Oktoberfest 2022</a></li>
+                <li><a href="galerie43.php">Herbstturnier 2022</a></li>
+                <li><a href="galerie41.php">Jubil&auml;umsturnier 2019</a></li>
+                <li><a href="galerie39.php">Herbstturnier 2019</a></li>
+                <li><a href="galerie36.php">Badische Meisterschaften 2019</a></li>
+                <li><a href="galerie34.php">Fr&uuml;hlingsturnier 2019</a></li>
+            </ul>
+	        </li>
+
+            <li><a href="liga.php">Liga&nbsp;</a></li>
+
+            <li><span>Einsteiger</span>
+            <ul>
+                <li><a href="beginners.php">Informationen zum Beginn</a></li>
+                <li><a href="images/regeln.pdf"><img src="images/pdf.gif"alt="pdf download" title="pdf download"/> &nbsp;&nbsp;so geht das Spiel</a></li>
+            </ul>
+            </li>
+
+            <li><a href="links.php">Links&nbsp;</a></li>
+
+            <li><span>Dokumente</span>
+            <ul>
+                <li><a href="dokumente.php">Dokumente</a></li>
+                <li><a href="archiv.php">Archiv</a> 
+            </ul>
+        	</li>
+
+            <li><a href="kontakt.php">Kontakt&nbsp;</a></li>
+
+            <li><span>Diverses</span>
+            <ul>
+                <li><a href="meinung.php">Leserbrief zum Boulodrome</a></li>
+                <li><a href="hexen.php">els&auml;sser Hexen</a></li>
+	            <li><a target="_blank"href="https://tsg-woersdorf-petanque.de/maenner-mit-putzlappen/">Männer mit Putzlappen</a></li>
+            </ul>
+            </li>
+    </div>
+
