fa25e156bfb28e3ff9714ddb5e8dcbea3e17ad3e
JoachimgGJKopp <JoachimGJ.Kopp@gmail.com>
2024-08-09 15:11:43 +0200

    Index responsiv

--- index.php
+++ index.php
@@ -24,18 +24,16 @@
         <img src="images/TeasHome.jpg" title="Frühling auf dem Platz" alt="Frühling auf dem Platz">
     </teaser>
 
-    <buttons>    
-        <Turnierplan id="turnierplan">
-            <div><a href="images/Turnierplan2024.pdf"><img src="images/WebTurnierplan2024klein.gif" 
-            alt="Turnierplan 2024 des BPV-Freiburg" onmouseover="src='images/WebTurnierplan2024kleinA.gif'" 
-            onmouseout="src='images/WebTurnierplan2024klein.gif'" /></a></div>
-        </Turnierplan>
+    <Turnierplan id="turnierplan">
+        <div><a href="images/Turnierplan2024.pdf"><img src="images/WebTurnierplan2024klein.gif" 
+        alt="Turnierplan 2024 des BPV-Freiburg" onmouseover="src='images/WebTurnierplan2024kleinA.gif'" 
+        onmouseout="src='images/WebTurnierplan2024klein.gif'" /></a></div>
+    </Turnierplan>
 
-        <Sparkasse id="sparkasse">   
-            <div><a target="_blank" href="http://www.sparkasse-freiburg.de/"><img src="images/Logospark.jpg" 
-            alt="Logo der Sparkasse Freiburg" title="Logo der Sparkasse Freiburg" /></a></div>
-        </Sparkasse>
-    </buttons>
+    <Sparkasse id="sparkasse">   
+        <div><a target="_blank" href="http://www.sparkasse-freiburg.de/"><img src="images/Logospark.jpg" 
+        alt="Logo der Sparkasse Freiburg" title="Logo der Sparkasse Freiburg" /></a></div>
+    </Sparkasse>
 
     <main>
 
